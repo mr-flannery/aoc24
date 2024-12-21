833be41951623f16a6cf7fa9d5961991fecffd7c
Dennis Hempfing <dennis.hempfing@sap.com>
2024-12-21 16:57:59 +0100

    wip

--- src/day13.py
+++ src/day13.py
@@ -13,67 +13,60 @@ def read_input(day, sample=False):
 
   return games
 
+def run_game(x,y, a_x, a_y, b_x, b_y):
+  x_temp = x
+  y_temp = y
+  a_counter = 0
+  b_counter = 0
+  if x % b_x == 0 and y % b_y == 0:
+    b_counter += x // b_x
+    return a_counter, b_counter
+  while True:
+    if x_temp % b_x == 0 and y_temp % b_y == 0:
+      # can there be scenarios where it is a better move to contiue using the same button?
+      a_counter_2, b_counter_2 = run_game(x_temp, y_temp, a_x, a_y, b_x, b_y)
+      if a_counter_2 is not None:
+        a_counter += a_counter_2
+        b_counter += b_counter_2
+        return a_counter, b_counter
+      else:
+        b_counter += x_temp // b_x
+        return a_counter, b_counter
+    if x_temp == 0 and y_temp == 0:
+      return a_counter, b_counter
+    if x_temp < 0 or y_temp < 0:
+      return None, None
+    x_temp -= a_x
+    y_temp -= a_y
+    a_counter += 1
+
+def solve(game):
+  a_x, a_y, b_x, b_y, x, y = game
+
+  a1_counter, b1_counter = run_game(x, y, a_x, a_y, b_x, b_y)
+  b2_counter, a2_counter = run_game(y, x, b_y, b_x, a_y, a_x)
+
+  if a1_counter is not None and (a1_counter > 100 or b1_counter > 100):
+    a1_counter, b1_counter = None, None
+  if a2_counter is not None and (a2_counter > 100 or b2_counter > 100):
+    a2_counter, b2_counter = None, None
+
+  if a1_counter is None and a2_counter is None:
+    return 0
+  elif a1_counter is None:
+    return 3*a2_counter+b2_counter
+  elif a2_counter is None:
+    return 3*a1_counter+b1_counter
+  else:
+    return min(3*a1_counter+b1_counter, 3*a2_counter+b2_counter)
+
 def part1():
   input = read_input(13, False)
 
   total_tokens = 0
 
   for game in input:
-    a_x, a_y, b_x, b_y, x, y = game
-
-    a_cost = (a_x**2 + a_y**2)**(1/2)
-    b_cost = (b_x**2 + b_y**2)**(1/2)
-    if a_cost > b_cost:
-      x_temp = x
-      y_temp = y
-      a_counter = 0
-      b_counter = 0
-      if x % b_x == 0 and y % b_y == 0:
-        b_counter += x // b_x
-        total_tokens += b_counter * 3
-        print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-        continue
-      while True:
-        if x_temp % b_x == 0 and y_temp % b_y == 0:
-          b_counter += x_temp // b_x
-          total_tokens += a_counter * 3 + b_counter
-          print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-          break
-        if x_temp == 0 and y_temp == 0:
-          total_tokens += a_counter * 3 + b_counter
-          print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-          break
-        if x_temp < 0 or y_temp < 0:
-          break
-        x_temp -= a_x
-        y_temp -= a_y
-        a_counter += 1
-    else:
-      x_temp = x
-      y_temp = y
-      a_counter = 0
-      b_counter = 0
-      if x % a_x == 0 and y % a_y == 0:
-        a_counter += x // a_x
-        total_tokens += a_counter * 3
-        print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-        continue
-      while True:
-        if x_temp % a_x == 0 and y_temp % a_y == 0:
-          a_counter += x_temp // a_x
-          total_tokens += a_counter * 3 + b_counter
-          print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-          break
-        if x_temp == 0 and y_temp == 0:
-          total_tokens += a_counter * 3 + b_counter
-          print(f"reached {x}/{y} with {a_counter} steps of {a_x}/{a_y} and {b_counter} steps of {b_x}/{b_y}, at cost of {a_counter * 3 + b_counter}")
-          break
-        if x_temp < 0 or y_temp < 0:
-          break
-        x_temp -= b_x
-        y_temp -= b_y
-        b_counter += 1
-      
+    total_tokens += solve(game)
   
   return total_tokens
 
