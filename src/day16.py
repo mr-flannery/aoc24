ee43818a93f72a6b27b45e558d7b5ffe56a4ce74
Dennis Hempfing <dennis.hempfing@sap.com>
2024-12-26 10:10:32 +0100

    wip 16.1

--- src/day16.py
+++ src/day16.py
@@ -2,6 +2,7 @@ import copy
 from functools import cache
 from itertools import chain
 import re
+from queue import PriorityQueue
 
 def read_input(day, sample=False):
   sample_or_real = '.sample' if sample else ''
@@ -11,78 +12,71 @@ def read_input(day, sample=False):
 
   maze = [list(line) for line in lines]
   return maze
-
-def move(maze, row, col, visited, score, direction):
-  if maze[row][col] == 'E':
-    return score
-
-  if (row, col) in visited:
-    return 999_999_999_999_999
-  
-  visited.add((row, col))
-
-  match direction:
-    case 'N':
-      next_scores = []
-      if maze[row-1][col] == '.':
-        next_scores.append(move(maze, row-1, col, visited, score, 'N'))
-      if maze[row][col-1] == '.':
-        next_scores.append(move(maze, row, col-1, visited, score, 'W')+1000)
-      if maze[row][col+1] == '.':
-        next_scores.append(move(maze, row, col+1, visited, score, 'E')+1000)
-      if next_scores:
-        return min(next_scores)
-      else:
-        return 999_999_999_999_999
-    case 'S':
-      next_scores = []
-      if maze[row+1][col] == '.':
-        next_scores.append(move(maze, row+1, col, visited, score, 'S'))
-      if maze[row][col-1] == '.':
-        next_scores.append(move(maze, row, col-1, visited, score, 'W')+1000)
-      if maze[row][col+1] == '.':
-        next_scores.append(move(maze, row, col+1, visited, score, 'E')+1000)
-      if next_scores:
-        return min(next_scores)
-      else:
-        return 999_999_999_999_999
-    case 'E': 
-      next_scores = []
-      if maze[row][col+1] == '.':
-        next_scores.append(move(maze, row, col+1, visited, score, 'E'))
-      if maze[row-1][col] == '.':
-        next_scores.append(move(maze, row-1, col, visited, score, 'N')+1000)
-      if maze[row+1][col] == '.':
-        next_scores.append(move(maze, row+1, col, visited, score, 'S')+1000)
-      if next_scores:
-        return min(next_scores)
-      else:
-        return 999_999_999_999_999
-    case 'W':
-      next_scores = []
-      if maze[row][col-1] == '.':
-        next_scores.append(move(maze, row, col-1, visited, score, 'W'))
-      if maze[row-1][col] == '.':
-        next_scores.append(move(maze, row-1, col, visited, score, 'N')+1000)
-      if maze[row+1][col] == '.':
-        next_scores.append(move(maze, row+1, col, visited, score, 'S')+1000)
-      if next_scores:
-        return min(next_scores)
-      else:
-        return 999_999_999_999_999
-
   
 def part1():
-  input = read_input(16, True)
+  input = read_input(16, False)
 
   current_row, current_col = None, None
+  exit_row, exit_col = None, None
   for row in range(len(input)):
     for col in range(len(input[row])):
       if input[row][col] == 'S':
         current_col = col
         current_row = row
+      if input[row][col] == 'E':
+        exit_col = col
+        exit_row = row
+        input[row][col] = '.'
 
-  return move(input, current_row, current_col, set(), 0, 'E')
+  queue = [(current_row, current_col, 0, 'E', set())]
+
+  results = []
+
+  while queue:
+    row, col, score, direction, visited = queue.pop(0)
+    
+    if (row, col) == (exit_row, exit_col):
+      results.append(score)
+
+    if (row, col) in visited:
+      continue
+
+    if results and score >= min(results):
+      continue
+    
+    visited.add((row, col))
+
+    match direction:
+      case 'N':
+        if input[row-1][col] == '.':
+          queue.append((row-1, col, score + 1, 'N', copy.deepcopy(visited)))
+        if input[row][col-1] == '.':
+          queue.append((row, col-1, score + 1001, 'W', copy.deepcopy(visited)))
+        if input[row][col+1] == '.':
+          queue.append((row, col+1, score + 1001, 'E', copy.deepcopy(visited)))
+      case 'S':
+        if input[row+1][col] == '.':
+          queue.append((row+1, col, score + 1, 'S', copy.deepcopy(visited)))
+        if input[row][col-1] == '.':
+          queue.append((row, col-1, score + 1001, 'W', copy.deepcopy(visited)))
+        if input[row][col+1] == '.':
+          queue.append((row, col+1, score + 1001, 'E', copy.deepcopy(visited)))
+      case 'E': 
+        if input[row][col+1] == '.':
+          queue.append((row, col+1, score + 1, 'E', copy.deepcopy(visited)))
+        if input[row-1][col] == '.':
+          queue.append((row-1, col, score + 1001, 'N', copy.deepcopy(visited)))
+        if input[row+1][col] == '.':
+          queue.append((row+1, col, score + 1001, 'S', copy.deepcopy(visited)))
+      case 'W':
+        if input[row][col-1] == '.':
+          queue.append((row, col-1, score + 1, 'W', copy.deepcopy(visited)))
+        if input[row-1][col] == '.':
+          queue.append((row-1, col, score + 1001, 'N', copy.deepcopy(visited)))
+        if input[row+1][col] == '.':
+          queue.append((row+1, col, score + 1001, 'S', copy.deepcopy(visited)))
+
+  return min(results)
   
 
 def part2():
